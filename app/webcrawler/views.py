bc430438cd33104ec2b8b17539b8a64d8cc7061a
Abhi06 <abhinandan06@gmail.com>
2023-05-20 07:38:09 -0700

    Add files via upload
    Added additional changes for storing vectors into Pinecone DB.

--- app/webcrawler/views.py
+++ app/webcrawler/views.py
@@ -1,11 +1,13 @@
 import os
 from . import webcrawler_blueprint
-from .utilities import crawl, prep_df, create_context, answer_question
+from .utilities import crawl, prep_df, answer_question
 import openai
 from flask import Flask, redirect, render_template, request, url_for
 
-domain ="seekingalpha.com"
-full_url = "https://seekingalpha.com/"
+processed_df_path = 'app/webcrawler/processed/embeddings.csv'
+domain = "mountainviewbrewing.ca"
+full_url = "https://mountainviewbrewing.ca/"
+
 crawl(full_url)
 df = prep_df(domain)
 
@@ -13,7 +15,7 @@ df = prep_df(domain)
 def index():
     if request.method == "POST":
         question = request.form["question"]
-        result = answer_question(df, model="ada", question=question, max_len=1800, size="ada", debug=False,max_tokens=150, stop_sequence=None)
+        result = answer_question(question=question)
         return redirect(url_for("webcrawler.index", result=result))
 
     result = request.args.get("result")
